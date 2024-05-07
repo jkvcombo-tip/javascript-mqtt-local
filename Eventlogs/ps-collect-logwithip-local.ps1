cff406584fbf6aae542a0046c28359672d50de04
jkvcombo-tip <qjkvcombo@tip.edu.ph>
2024-05-07 14:26:47 +0800

    add powershell script for local log colletion with ip name on csv file

---
+++ Eventlogs/ps-collect-logwithip-local.ps1
@@ -0,0 +1,16 @@
+# Retrieve all events from the System event log
+$events = Get-WinEvent -LogName System
+
+# Define the path to the folder
+$folderPath = "C:\Eventlogs"
+
+# Check if the folder exists, if not, create it
+if (-not (Test-Path -Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory -Force
+}
+
+# Get the IP address of the machine
+$ip = (Test-Connection -ComputerName (hostname) -Count 1).IPv4Address.IPAddressToString
+
+# Export events to CSV file and rename it with the IP address
+$events | Export-Csv -Path "$folderPath\$ip.csv" -NoTypeInformation
